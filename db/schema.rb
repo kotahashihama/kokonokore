20f2c165fc2f3d8288e045b04c247b5eefff5265
Kota Hashihama <kotahashihama@gmail.com>
2019-04-08 05:36:58 +0000

    Modify files to fix bugs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190408000753) do
+ActiveRecord::Schema.define(version: 20190408042431) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string   "name",        null: false
@@ -54,10 +54,10 @@ ActiveRecord::Schema.define(version: 20190408000753) do
     t.string   "name"
     t.string   "email"
     t.text     "description"
-    t.string   "password"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "image_url"
+    t.string   "password"
   end
 
 end
