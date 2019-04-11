ee9ed6cf61d1400d4766ad460a646e689c5ed7a9
Kota Hashihama <kotahashihama@gmail.com>
2019-04-11 04:18:22 +0000

    Rename CarrierWave file

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,5 +7,12 @@ if Rails.env.production?
       :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
     config.fog_directory = ENV['S3_BUCKET']
+    
+    # 公開・非公開の切り替え
+    config.fog_public     = true
+    # キャッシュの保存期間
+    config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" }
   end
 end
+
+
