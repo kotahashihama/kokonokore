6e57a76a4166f70230bb9576bb92ddb66f2a17b5
Kota Hashihama <kotahashihama@gmail.com>
2019-04-11 05:45:31 +0000

    Update README

--- app/models/food.rb
+++ app/models/food.rb
@@ -4,5 +4,5 @@ class Food < ApplicationRecord
   validates :shop_name, presence: true
   validates :description, presence: true, length: {maximum: 140}
   validates :user_id, presence: true
-  validates :picture, presence: true
+  # validates :picture, presence: true
 end
